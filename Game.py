fae1e2f002336869d13c876cc52b64979557d49d
Chrome 111.0.0.0, Windows 10 <126879822+ArriesIsHier@users.noreply.github.com>
2023-07-10 08:46:52 +0200

    v2
    v2

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 import pygame
 import webbrowser
 import random
+import pygame.freetype  # Import the freetype module.
 
 # Initialize Pygame
 pygame.init()
@@ -14,7 +15,7 @@ POWERUP_TIME = 10  # Seconds that the speed powerup lasts
 GAME_TIME = 60  # Seconds that the player has to reach the goal
 
 # Create the game window
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 
 # Create a player, goal, obstacles, and power-ups
 player = pygame.Rect(WIDTH // 2, HEIGHT // 2, 50, 50)
@@ -26,18 +27,37 @@ player_health = 100
 powerup_active = False
 powerup_time_remaining = 0
 game_time_remaining = GAME_TIME
+score = 0
+
 
 def rickroll():
     webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
+
+def main_menu():
+    menu_font = pygame.freetype.Font(None, 50)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    game()
+
+        screen.fill((0, 0, 0))
+        menu_font.render_to(screen, (WIDTH // 2 - 100, HEIGHT // 2 - 50), "Press SPACE to start", (255, 255, 255))
+        menu_font.render_to(screen, (WIDTH // 2 - 150, HEIGHT // 2 + 50), "Made by ArriesIsHier", (255, 255, 255))
+        pygame.display.flip()
+
+
 def game():
-    global player_health, powerup_active, powerup_time_remaining, game_time_remaining
+    global player_health, powerup_active, powerup_time_remaining, game_time_remaining, score
     clock = pygame.time.Clock()
     running = True
     start_ticks = pygame.time.get_ticks()
 
     while running:
-        seconds = (pygame.time.get_ticks()-start_ticks)/1000
+        seconds = (pygame.time.get_ticks() - start_ticks) / 1000
         game_time_remaining = GAME_TIME - seconds
 
         if game_time_remaining <= 0:
@@ -61,6 +81,7 @@ def game():
 
         if player.colliderect(goal):
             running = False
+            score += 100  # Bonus points for reaching the goal
             rickroll()
 
         for obstacle in obstacles:
@@ -75,9 +96,10 @@ def game():
                 powerups.remove(powerup)
                 powerup_active = True
                 powerup_time_remaining = POWERUP_TIME
+                score += 10  # Points for collecting a powerup
 
         if powerup_active:
-            powerup_time_remaining -= 1/60  # Decrease the time remaining by the time since the last frame
+            powerup_time_remaining -= 1 / 60  # Decrease the time remaining by the time since the last frame
             if powerup_time_remaining <= 0:
                 powerup_active = False
 
@@ -88,11 +110,31 @@ def game():
             pygame.draw.rect(screen, (255, 255, 255), obstacle)
         for powerup in powerups:
             pygame.draw.rect(screen, (0, 0, 255), powerup)
-        pygame.display.flip()
 
+        # Display player status
+        status_font = pygame.freetype.Font(None, 30)
+        status_font.render_to(screen, (10, 10), f"Health: {player_health}", (255, 255, 255))
+        status_font.render_to(screen, (10, 40), f"Power Up: {'Active' if powerup_active else 'Inactive'}",
+                              (255, 255, 255))
+        status_font.render_to(screen, (10, 70), f"Score: {score}", (255, 255, 255))
+
+        pygame.display.flip()
         clock.tick(60)
 
-    pygame.quit()
+    # Display game over message and score
+    game_over_font = pygame.freetype.Font(None, 50)
+
+
+# Display game over message and score
+game_over_font = pygame.freetype.Font(None, 50)
+game_over_font.render_to(screen, (WIDTH // 2 - 100, HEIGHT // 2 - 50), "Game Over", (255, 255, 255))
+game_over_font.render_to(screen, (WIDTH // 2 - 100, HEIGHT // 2), f"Final Score: {score}", (255, 255, 255))
+pygame.display.flip()
+
+# Wait a few seconds before returning to the main menu
+pygame.time.wait(3000)
 
 if __name__ == "__main__":
-    game()
+    main_menu()
+
+pygame.quit()
